96878f8dd85ebb1b01aa51106c0045e1e36762ca
jotajotinha02 <julionascimentoaz7@gmail.com>
2025-08-04 03:31:19 -0300

    Add files via upload

---
+++ SaleTrack (1) (2)/SaleTrack/client/src/App.tsx
@@ -0,0 +1,28 @@
+import { Switch, Route } from "wouter";
+import { queryClient } from "./lib/queryClient";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "@/components/ui/toaster";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import Sales from "@/pages/sales";
+
+function Router() {
+  return (
+    <Switch>
+      <Route path="/" component={Sales}/>
+      <Route path="*" component={Sales} />
+    </Switch>
+  );
+}
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Router />
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
